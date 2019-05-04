ff02af4ee4310c6e0c1d7778521c07bf2e315420
farhanprasla10 <farhan.prasla31@gmail.com>
2019-05-04 22:17:38 +0000

    Dashboard

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -22,7 +22,7 @@ class Instructor::SectionsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    if current_section.course.course.user != current_user
+    if current_section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
